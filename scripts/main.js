48a11bb8e25b944f109602cde0f58ac31eea8252
Mark Polovtsev <mpolovtsev@gmail.com>
2021-01-30 10:44:11 +0300

    feat: добавил js логику

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+let profile = document.querySelector('.profile');
+let profileEditButton = profile.querySelector('.profile__edit-button');
+let profileTitle = profile.querySelector('.profile__title');
+let profileSubtitle = profile.querySelector('.profile__subtitle');
+
+let popup = document.querySelector('.popup');
+let popupTitle = popup.querySelector('.popup__title');
+let popupSubtitle = popup.querySelector('.popup__subtitle');
+let popupSaveButton = popup.querySelector('.popup__submit-button');
+let popupCloseButton = popup.querySelector('.popup__close-button');
+
+let togglePopup = () => {
+  popup.classList.toggle('popup_visible');
+}
+
+profileEditButton.addEventListener('click', () => {
+  popupTitle.value = profileTitle.innerText;
+  popupSubtitle.value = profileSubtitle.innerText;
+  togglePopup();
+});
+
+popupCloseButton.addEventListener('click', () => {
+  togglePopup();
+});
+
+popupSaveButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  profileTitle.innerText = popupTitle.value;
+  profileSubtitle.innerText = popupSubtitle.value;
+  togglePopup();
+});
+
+
