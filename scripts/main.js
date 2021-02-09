c9e32699bf3a69ac0ed58983ef4f123ca6881f8a
Mark Polovtsev <mpolovtsev@gmail.com>
2021-02-09 15:51:26 +0300

    feat: Шесть карточек из коробки

--- scripts/main.js
+++ scripts/main.js
@@ -29,3 +29,50 @@ let saveProfile = (e) => {
 profileEditButton.addEventListener('click', editProfile);
 popupCloseButton.addEventListener('click', togglePopup);
 popupForm.addEventListener('submit', saveProfile);
+
+
+const cards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardsContainer = document.querySelector('.cards');
+const cardTemplate = document.querySelector('#card');
+
+const renderCards = () => {
+  const cardsElements = cards.map((card) => {
+    const newCard = cardTemplate.content.cloneNode(true);
+    const newCardImage = newCard.querySelector('.cards__image');
+    const newCardHeading = newCard.querySelector('.cards__title');
+    newCardImage.src = card.link;
+    newCardImage.alt = card.name;
+    newCardHeading.textContent = card.name;
+    return newCard;
+  });
+
+  cardsContainer.append(...cardsElements);
+};
+
+renderCards();
